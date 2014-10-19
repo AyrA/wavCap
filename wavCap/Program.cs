7f5a541a3058d4068dfa6989dda8dbf85087e932
Kevin Gut <kevin.gut@bitm.sg>
2014-10-19 03:48:38 +0200

    demonstration of working with audio devices. uses the DLL project.

---
+++ wavCap/Program.cs
@@ -0,0 +1,187 @@
+﻿//#define BLOCK
+
+using System;
+using WaveLib;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.IO;
+using System.Collections.Generic;
+
+namespace wavCap
+{
+    class Program
+    {
+        private const int DEFAULT_SOUNDCARD = -1;
+        private const int BUFFERS = 32;
+        private const int BUFFERSIZE = 128;
+        private const int RATE = 8000;//8000;
+        private const int BITS = 8;
+        private const int CHANNELS = 1;
+
+        private const char C = '█';
+
+        private static int counter = 0;
+        private static int loopcount = 0;
+        private static List<byte[]> Buffers;
+
+        private static string code;
+        private static StackLang L;
+
+        static void Main(string[] args)
+        {
+            code = "B9*4B>&B5*7B>&|B3*92<B/&|]";
+            int pos = code.Length;
+            L = new StackLang();
+            Buffers = new List<byte[]>(BUFFERS);
+            for (int i = 0; i < BUFFERS; i++)
+            {
+                Buffers.Add(new byte[BUFFERSIZE]);
+            }
+
+            WaveFormat f = new WaveFormat(RATE, BITS, CHANNELS);
+            WaveOutPlayer plr = new WaveOutPlayer(DEFAULT_SOUNDCARD, f, BUFFERSIZE, BUFFERS, new BufferFillEventHandler(DataSent));
+
+            bool cont = true;
+
+            DisplayHelp();
+
+            while (cont)
+            {
+                display(code, pos);
+                ConsoleKeyInfo CKI = Console.ReadKey(true);
+                switch (CKI.Key)
+                {
+                    case ConsoleKey.Escape:
+                        cont = false;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (pos > 0)
+                        {
+                            pos--;
+                        }
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (pos < code.Length)
+                        {
+                            pos++;
+                        }
+                        break;
+                    case ConsoleKey.Insert:
+                        if (pos < code.Length)
+                        {
+                            code = code.Insert(pos, " ");
+                        }
+                        break;
+                    case ConsoleKey.Delete:
+                        if (pos < code.Length)
+                        {
+                            code = code.Remove(pos, 1);
+                        }
+                        break;
+                    case ConsoleKey.Backspace:
+                        if (pos > 0)
+                        {
+                            code = code.Remove(--pos, 1);
+                            Console.Write(' ');
+                        }
+                        break;
+                    default:
+                        if (CKI.KeyChar > 31 && CKI.KeyChar < 127)
+                        {
+                            if (pos < code.Length)
+                            {
+                                code = code.Remove(pos, 1);
+                                code = code.Insert(pos++, CKI.KeyChar.ToString().ToUpper());
+                            }
+                            else
+                            {
+                                code += CKI.KeyChar.ToString().ToUpper();
+                                pos++;
+                            }
+                        }
+                        break;
+                }
+            }
+            plr.Dispose();
+            Console.WriteLine();
+            Console.WriteLine("Finished");
+        }
+
+        private static void DisplayHelp()
+        {
+            Console.WriteLine("\r\n");
+            Console.WriteLine("Assuming the Top value of the stack is y and the second top value is x");
+            //─│┌┐└┘┼┴┬┤├
+            Console.WriteLine(@"
+┌──┬─────────────────────────────────────────────────────────────────┐
+│A │ put the counter on stack          (resets for each buffer)      │
+│B │ put the global counter on stack   (increments indefinitely)     │
+│C │ put the loop counter on stack     (resets on each buffer cycle) │
+├──┼────────────┬─────────────────┬───┬────────────┬─────────────────┤
+│+ │ Add        │ x + y           │ - │ Subtract   │ x - y           │
+│* │ Multiply   │ x * y           │ / │ Divide     │ x / y           │
+│% │ Modulo     │ x % y           │ ! │ Pow        │ Pow(x,y)        │
+├──┼────────────┼─────────────────┼───┼────────────┼─────────────────┤
+│> │ Shift R    │ x >> y          │ < │ Shift L    │ x << y          │
+├──┼────────────┼─────────────────┼───┼────────────┼─────────────────┤
+│[ │ Increment  │ y + 1           │ ] │ Decrement  │ y - 1           │
+├──┼────────────┼─────────────────┼───┼────────────┼─────────────────┤
+│| │ Or         │ x | y           │ & │ And        │ x & y           │
+│^ │ Xor        │ x ^ y           │ ~ │ Not        │ ~ y             │
+├──┴────────────┴─────────────────┴───┴────────────┴─────────────────┤
+│Empty stack will return 0 on operations whenever possible.          │
+│Division by zero is prevented by supplying 1 to y if needed.        │
+├────────────────────────────────────────────────────────────────────┤
+│Values that affect A and C:                                         │
+│                                                                    │
+│Number of Buffers: {0,5}    (C is smaller than this)                │
+│Buffer size:       {1,5}    (A is smaller than this)                │
+└────────────────────────────────────────────────────────────────────┘
+", BUFFERS, BUFFERSIZE);
+        }
+
+        private static void display(string code, int pos)
+        {
+            Console.SetCursorPosition(0, 1);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(string.Empty.PadRight(Console.BufferWidth - 1));
+            Console.CursorLeft = 0;
+            string temp = string.Format("Form: {0}", L.eval(code));
+            if (temp.Length > Console.BufferWidth - 1)
+            {
+                temp = temp.Substring(0, Console.BufferWidth - 4) + "...";
+            }
+            Console.Write(temp);
+            Console.SetCursorPosition(0, 0);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(string.Empty.PadRight(Console.BufferWidth - 1));
+            Console.CursorLeft = 0;
+            Console.Write("Code: {0} ", code);
+            Console.CursorLeft--;
+            Console.ForegroundColor = ConsoleColor.Red;
+            if (pos < code.Length)
+            {
+                Console.CursorLeft = pos + 6;
+                Console.Write(code[pos]);
+            }
+            else
+            {
+                Console.Write(C);
+            }
+            Console.ResetColor();
+            Console.CursorLeft--;
+        }
+
+        private static void DataSent(IntPtr start, int size)
+        {
+            int i;
+            for (i = 0; i < BUFFERSIZE; i++)
+            {
+                int sample = L.Process(code, new string[] { "A" + i.ToString(), "B" + counter.ToString(), "C" + (loopcount % BUFFERS).ToString() });
+                Buffers[loopcount % BUFFERS][i] = (byte)sample;
+                counter++;
+            }
+            Marshal.Copy(Buffers[loopcount % BUFFERS], 0, start, size);
+        }
+    }
+}
---
+++ wavCap/clsStackLang.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace wavCap
+{
+    public class StackLang
+    {
+        private Stack<int> S;
+        private Stack<string> SS;
+
+        public StackLang()
+        {
+        }
+
+        public string eval(string Code)
+        {
+            SS = new Stack<string>();
+            Hashtable HT = new Hashtable();
+            foreach (char c in Code)
+            {
+                switch (c)
+                {
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        //push number onto stack
+                        SS.Push(c.ToString());
+                        break;
+                    case '+':
+                        //add
+                        SS.Push(string.Format("({0}+{1})", getS(), getS()));
+                        break;
+                    case '-':
+                        //subtract
+                        SS.Push(string.Format("({0}-{1})", getS(), getS()));
+                        break;
+                    case '*':
+                        //multi
+                        SS.Push(string.Format("({0}*{1})", getS(), getS()));
+                        break;
+                    case '/':
+                        //div
+                        SS.Push(string.Format("({0}/{1})", getS(), getS()));
+                        break;
+                    case '%':
+                        //modulo
+                        SS.Push(string.Format("({0}%{1})", getS(), getS()));
+                        break;
+                    case '>':
+                        //shift right
+                        SS.Push(string.Format("({0}>>{1})", getS(), getS()));
+                        break;
+                    case '<':
+                        //shift left
+                        SS.Push(string.Format("({0}<<{1})", getS(), getS()));
+                        break;
+                    case '&':
+                        //and
+                        SS.Push(string.Format("({0}&{1})", getS(), getS()));
+                        break;
+                    case '|':
+                        //or
+                        SS.Push(string.Format("({0}|{1})", getS(), getS()));
+                        break;
+                    case '^':
+                        //xor
+                        SS.Push(string.Format("({0}^{1})", getS(), getS()));
+                        break;
+                    case '~':
+                        //complement
+                        SS.Push(string.Format("~{0}", getS()));
+                        break;
+                    case '[':
+                        //increment
+                        SS.Push(string.Format("({0}+1)", getS()));
+                        break;
+                    case ']':
+                        //decrement
+                        SS.Push(string.Format("({0}-1)", getS()));
+                        break;
+                    case '!':
+                        //power
+                        SS.Push(string.Format("Pow({0},{1})", getS(), getS()));
+                        break;
+                    case ' ':
+                        //NOOP
+                        break;
+                    default:
+                        //get predefined value
+                        SS.Push(c.ToString().ToUpper());
+                        break;
+                }
+            }
+            string s = getS();
+            if (s.Length > 2)
+            {
+                return SS.Count == 0 ? s.Substring(1, s.Length - 2) : s.Substring(1, s.Length - 2) + " STACK NOT EMPTY!";
+            }
+            return SS.Count==0?s:s + " STACK NOT EMPTY!";
+        }
+
+        public int Process(string Code, string[] Vars)
+        {
+            S = new Stack<int>();
+            Hashtable HT = new Hashtable();
+            foreach (string Var in Vars)
+            {
+                HT.Add(Var[0], int.Parse(Var.Substring(1)));
+            }
+            foreach (char c in Code)
+            {
+                switch (c)
+                {
+                    case '0':
+                    case '1':
+                    case '2':
+                    case '3':
+                    case '4':
+                    case '5':
+                    case '6':
+                    case '7':
+                    case '8':
+                    case '9':
+                        //push number onto stack
+                        S.Push(c - 48);
+                        break;
+                    case '+':
+                        //add
+                        S.Push(get() + get());
+                        break;
+                    case '-':
+                        //subtract
+                        S.Push(get() - get());
+                        break;
+                    case '*':
+                        //multi
+                        S.Push(get() * get());
+                        break;
+                    case '/':
+                        //div
+                        S.Push(get() / get(1));
+                        break;
+                    case '%':
+                        //modulo
+                        S.Push(get() % get(1));
+                        break;
+                    case '>':
+                        //shift right
+                        S.Push(get() >> get());
+                        break;
+                    case '<':
+                        //shift left
+                        S.Push(get() << get());
+                        break;
+                    case '&':
+                        //and
+                        S.Push(get() & get());
+                        break;
+                    case '|':
+                        //or
+                        S.Push(get() | get());
+                        break;
+                    case '^':
+                        //xor
+                        S.Push(get() ^ get());
+                        break;
+                    case '~':
+                        //complement
+                        S.Push(~get());
+                        break;
+                    case '[':
+                        //increment
+                        S.Push(get() + 1);
+                        break;
+                    case ']':
+                        //decrement
+                        S.Push(get() - 1);
+                        break;
+                    case '!':
+                        //power
+                        S.Push((int)Math.Pow(get(), get()));
+                        break;
+                    case ' ':
+                        //NOOP
+                        break;
+                    default:
+                        //get predefined value
+                        S.Push(HT[c] != null ? (int)HT[c] : 0);
+                        break;
+                }
+            }
+            return get();
+        }
+
+        private int get()
+        {
+            return get(0);
+        }
+
+        private int get(int onZeroOrEmpty)
+        {
+            if (S.Count == 0)
+            {
+                return onZeroOrEmpty;
+            }
+            if (S.Peek() == 0)
+            {
+                S.Pop();
+                return onZeroOrEmpty;
+            }
+            return S.Pop();
+        }
+
+        private string getS()
+        {
+            if (SS.Count == 0)
+            {
+                return "_";
+            }
+            return SS.Pop();
+        }
+    }
+}
